0453fa6af316978ae1d793f0b7f342f728f6c9f8
Riley Flynn <riley@rileyflynn.me>
2019-08-31 16:41:21 -0230

    Added graphing

---
+++ grapher.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+import networkx as nx
+
+with open("sponsors.json") as f:
+    sponsors = json.load(f)
+
+sponsor_pairs = [(key, value) for key in sponsors.keys() for value in sponsors[key]]
+
+graph = nx.Graph()
+
+for pair in sponsor_pairs:
+    graph.add_node(pair[0], color="#BA68C8")
+    graph.add_node(pair[1], color="#FFD54F")
+    graph.add_edge(*pair)
+
+agraph = nx.nx_agraph.to_agraph(graph)
+agraph.layout("sfdp")
+agraph.draw("sponsors.png")
--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@ import re
 from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
+blacklisted_sponsors = {"patreon", "bit.ly"}
+
 load_dotenv()
 
 client = build("youtube", "v3", developerKey=os.environ["YOUTUBE_API_KEY"])
@@ -16,20 +18,41 @@ sponsors = {}
 
 for channel in channels:
     sponsors[channel["display_name"]] = []
-    videos = (
+    channel_videos = []
+    results = (
         client.search()
         .list(channelId=channel["id"], part="snippet", order="date", maxResults=50)
         .execute()
-        .get("items", [])
     )
+    channel_videos += results["items"]
+    while "nextPageToken" in results:
+        results = (
+            client.search()
+            .list(
+                channelId=channel["id"],
+                part="snippet",
+                order="date",
+                maxResults=50,
+                pageToken=results["nextPageToken"],
+            )
+            .execute()
+        )
+        channel_videos += results["items"]
 
-    for video in videos:
+    for video in channel_videos:
         for regex in channel["sponsor_regexes"]:
             print(video["snippet"]["description"])
             sponsor = re.findall(regex, video["snippet"]["description"])
             if sponsor:
                 sponsor = sponsor[0].lower().replace("www.", "")
-            if sponsor and sponsor not in sponsors[channel["display_name"]]:
+            if (
+                sponsor
+                and sponsor not in sponsors[channel["display_name"]]
+                and not any(
+                    sponsor in blacklisted_sponsor
+                    for blacklisted_sponsor in blacklisted_sponsors
+                )
+            ):
                 sponsors[channel["display_name"]].append(sponsor)
 
 with open("sponsors.json", "w") as f:
